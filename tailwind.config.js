90ebe3019f0c003ee28d2faddea3226318910542
Shreyash Dhulrao <shreyashdhularao@gmail.com>
2024-09-01 20:55:29 +0530

    font update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ export default {
   theme: {
     extend: {
       fontFamily:{
-        Giarek: ['Bodoni Moda']
+        Giarek: ['Bodoni Moda'],
+        Raleway: ['Raleway']
       }
     },
   },
