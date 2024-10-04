8273ec263e3b131c57e29e71ebe02975fafa588c
Shreyash Dhulrao <shreyashdhularao@gmail.com>
2024-10-04 20:48:16 +0530

    work added

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import alphatype from './Images/Keyboard.jpg'
+import ReactImg from './Images/React.png'
+import TailwindImg from './Images/Tailwind.png'
+import FirebaseImg from './Images/Firebase.png'
+
 
 const Work = () => {
     return (
@@ -7,11 +11,17 @@ const Work = () => {
             <div>
                 <h2 class="mb-2 md:text-4xl text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center font-Giarek tracking-wider">Work</h2>
                 <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 w-full gap-3 p-5 font-["Raleway"]'>
-                    <a href="alphatype.vercel.app">
-                        <div class="max-w-sm bg-zinc-200 dark:bg-zinc-800 overflow-hidden rounded-xl">
+                    <a href="https://alphatype.vercel.app " target='_blank'>
+                        <div class="max-w-sm  bg-zinc-200 dark:bg-zinc-800 overflow-hidden rounded-xl">
                             <img className='w-fill' src={alphatype} alt="" />
                             <div class="p-5">
+                                <div className='flex w-full items-center gap-2'>
                                 <h5 class="mb-2 md:text-2xl text-lg font-bold tracking-tight text-gray-900 dark:text-white">Alpha Type </h5>
+                                <img src={ReactImg} alt="" className=' h-5' />
+                                <img src={TailwindImg} alt="" className=' h-4' />
+                                <img src={FirebaseImg} alt="" className=' h-6' />
+                                </div>
+                                <h5 class="mb-2 md:text-md text-md font-medium text-orange-600 dark:text-orange-500">React Js, Tailwind CSS, Firebase.</h5>
                                 <p class="mb-3 md:text-md text-sm font-normal text-gray-700 dark:text-gray-400">Alpha Type is your go-to platform to improve typing speed and accuracy. Whether you're a beginner or looking to refine your skills, Alpha Type provides personalized exercises and progress tracking to help you type faster and more efficiently.</p>
                             </div>
                         </div>
